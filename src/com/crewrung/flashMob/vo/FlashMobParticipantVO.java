e5ba36514fde8c1643075c6feb723d967eb26d13
kokoko12334 <gnjgnjgnj1@gmail.com>
2025-04-21 17:00:22 +0900

    feat: flashMobParticipantVO 구현

---
+++ src/com/crewrung/flashMob/vo/FlashMobParticipantVO.java
@@ -0,0 +1,46 @@
+package com.crewrung.flashMob.vo;
+
+import java.sql.Date;
+
+public class FlashMobParticipantVO {
+	private String userId;
+    private int flashMobNumber;
+    private Date applyDate;
+
+	public FlashMobParticipantVO(String userId, int flashMobNumber, Date applyDate) {
+		this.userId = userId;
+		this.flashMobNumber = flashMobNumber;
+		this.applyDate = applyDate;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public int getFlashMobNumber() {
+		return flashMobNumber;
+	}
+
+	public void setFlashMobNumber(int flashMobNumber) {
+		this.flashMobNumber = flashMobNumber;
+	}
+
+	public Date getApplyDate() {
+		return applyDate;
+	}
+
+	public void setApplyDate(Date applyDate) {
+		this.applyDate = applyDate;
+	}
+
+	@Override
+	public String toString() {
+		return "FlashMobParticipantVO [userId=" + userId + ", flashMobNumber=" + flashMobNumber + ", applyDate="
+				+ applyDate + "]";
+	}
+
+}
