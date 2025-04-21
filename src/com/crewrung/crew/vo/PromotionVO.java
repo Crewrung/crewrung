3be3cf1345a14a0a6a67a5661ee79ca0fddd66f7
dehetnim <jng3268@gmail.com>
2025-04-21 16:21:08 +0900

    promotionvo 구현

---
+++ src/com/crewrung/crew/vo/PromotionVO.java
@@ -0,0 +1,35 @@
+package com.crewrung.crew.vo;
+
+public class PromotionVO {
+	private String promotionMessage;
+	private String introduction;
+	
+	
+	public PromotionVO(String promotionMessage, String introduction) {
+		super();
+		this.promotionMessage = promotionMessage;
+		this.introduction = introduction;
+	}
+	
+	public String getPromotionMessage() {
+		return promotionMessage;
+	}
+	
+	public void setPromotionMessage(String promotionMessage) {
+		this.promotionMessage = promotionMessage;
+	}
+	
+	public String getIntroduction() {
+		return introduction;
+	}
+	
+	public void setIntroduction(String introduction) {
+		this.introduction = introduction;
+	}
+
+	@Override
+	public String toString() {
+		return "PromotionVO [promotionMessage=" + promotionMessage + ", introduction=" + introduction + "]";
+	}
+
+}
