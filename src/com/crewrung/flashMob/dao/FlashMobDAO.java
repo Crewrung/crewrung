7a39cc4cc5ee96ffabeec3a03b52c338d884f128
Uigeol <gamoon777@naver.com>
2025-04-22 17:37:59 +0900

    feat: flashMobDAO  모임 생성, 모임 조회, 댓글 등록, 댓글 조회 구현

--- src/com/crewrung/flashMob/dao/FlashMobDAO.java
+++ src/com/crewrung/flashMob/dao/FlashMobDAO.java
@@ -6,7 +6,9 @@ import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 
+import com.crewrung.flashMob.vo.FlashMobCommentVO;
 import com.crewrung.flashMob.vo.FlashMobMainViewVO;
+import com.crewrung.flashMob.vo.FlashMobParticipantVO;
 import com.crewrung.flashMob.vo.FlashMobVO;
 
 public class FlashMobDAO {
@@ -35,12 +37,45 @@ public class FlashMobDAO {
 	public int addFlashMob(FlashMobVO flashMobVO) {
 		int result = 0;
 		result = con.insert("flashMobMapper.addFlashMob", flashMobVO);
+		con.close();
 		return result;
 	}
 	
 	public FlashMobVO getFlashMobDetail(int flashMobNumber) {
 		FlashMobVO result = null;
 		result = con.selectOne("flashMobMapper.getFlashMobDetail", flashMobNumber);
+		con.close();
+		return result;
+	}
+	
+	//번개 모임 신청하기 add
+	public int addFlashMobParticipant(FlashMobParticipantVO flashMobParticipantVO) {
+		int result = 0;
+		result = con.insert("flashMobMapper.addFlashMobParticipant", flashMobParticipantVO);
+		con.close();
+		return result;
+	}
+
+	//번개 모임 수정하기 set
+	public int setFlashMobDetail(FlashMobVO flashMobVO) {
+		int result = 0;
+		result = con.update("flashMobMapper.setFlashMobDetail", flashMobVO);
+		con.close();
+		return result;
+	}
+	//번개 모임 댓글 조회 get
+	public List<FlashMobCommentVO> getAllFlashMobComments(int flashMobNumber) {
+		List<FlashMobCommentVO> result = new ArrayList<>();
+		result = con.selectList("flashMobMapper.getAllFlashMobComments", flashMobNumber);
+		con.close();
+		return result;
+	}
+	
+	//번개 모임 댓글 등록 add
+	public int addFlashMobComment(FlashMobCommentVO flashMobCommentVO) {
+		int result = 0;
+		result = con.insert("flashMobMapper.addFlashMobComment", flashMobCommentVO);
+		con.close();
 		return result;
 	}
 
